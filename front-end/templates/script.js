5a132299033d0209dde52ec3a4df928a28e322e7
Lucas Gontijo  de Moraes <164579296+Luucasgontijo@users.noreply.github.com>
2024-08-27 15:49:31 -0300

    f

---
+++ front-end/templates/script.js
@@ -0,0 +1,69 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const campos = "media_type,media_url,permalink";
+    const limite = 40;
+    const baseURL = `https://graph.instagram.com/me/media?fields=${campos}&access_token=${token}&limit=${limite}`;
+    
+    fetch(baseURL)
+        .then((response) => response.json())
+        .then((data) => { 
+            const container = document.querySelector(".container");
+    
+            data.data.forEach((img) => {
+                const mediaType = img.media_type;
+                const mediaUrl = img.media_url;
+                const permalink = img.permalink;
+                if (mediaType === "VIDEO") {
+                    // const video = document.createElement("video");
+                    // video.src = mediaUrl;
+                    // video.classList.add("container-video");
+                    // container.appendChild(video);
+                } else {
+                    const anchor = document.createElement("a");
+                    anchor.href = permalink;
+                    anchor.target = "_blank"; // Open in a new tab
+                    const image = document.createElement("img");
+                    image.src = mediaUrl;
+                    image.classList.add("container-img");
+                    anchor.appendChild(image);
+                    container.appendChild(anchor);
+                }
+            });
+
+            $('.responsive').slick({
+                dots: true,
+                arrows: true,
+                infinite: true,
+                speed: 300,
+                slidesToShow: 4,
+                slidesToScroll: 4,
+                responsive: [
+                    {
+                        breakpoint: 1024,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 3,
+                            infinite: true,
+                            dots: true,
+                            arrows: true,
+                        }
+                    },
+                    {
+                        breakpoint: 600,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2,
+                            arrows: true,
+                        }
+                    },
+                    {
+                        breakpoint: 480,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                            arrows: true,
+                        }
+                    }
+                ]
+            });
+        });
+});
